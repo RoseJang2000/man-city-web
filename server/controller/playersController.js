42656d027ecb4910284c80d96bb22fc30cfebc9d
Rose Jang <jangmi749@gmail.com>
2023-03-05 18:43:50 +0900

    query 틀릴 시 404 response

--- server/controller/playersController.js
+++ server/controller/playersController.js
@@ -4,6 +4,14 @@ module.exports = {
   getPlayers: (req, res) => {
     const { position } = req.query;
     if (position) {
+      if (
+        position !== 'forwards' &&
+        position !== 'midfielders' &&
+        position !== 'defenders' &&
+        position !== 'goalkeepers'
+      ) {
+        return res.status(404).send('Not Found!');
+      }
       console.log(position);
       const filterdPlayers = players[position];
       return res.status(200).json(filterdPlayers);

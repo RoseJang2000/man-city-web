62a3c853e2bd189ff3dad7ba6524e3cd866fcdcb
Rose Jang <jangmi749@gmail.com>
2023-03-30 12:47:33 +0900

    :sparkles: feat : 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home, Players } from 'pages';
+import GlobalStyle from 'styles/GlobalStyle';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/players" element={<Players />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+import Home from './Home';
+import Players from './Players';
+
+export { Home, Players };

0167f1fd790b07408b5d9829382d06f8470c164f
Rose Jang <jangmi749@gmail.com>
2023-04-01 19:07:02 +0900

    :sparkles: feat : NavLink 설정

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import styled, { css, keyframes } from "styled-components";
 
 const NavMenu = ({ aniMode }: { aniMode: boolean }) => {
@@ -11,7 +12,19 @@ const NavMenu = ({ aniMode }: { aniMode: boolean }) => {
   return (
     <>
       <NavBg></NavBg>
-      {menuRender && <NavContainer aniMode={aniMode}></NavContainer>}
+      {menuRender && (
+        <NavContainer aniMode={aniMode}>
+          <div className="nav-list-wrapper">
+            <NavList>
+              <NavLink to="/">home</NavLink>
+            </NavList>
+            <NavList>players</NavList>
+            <NavList>
+              <NavLink to="news">news</NavLink>
+            </NavList>
+          </div>
+        </NavContainer>
+      )}
     </>
   );
 };
@@ -48,6 +61,11 @@ const NavContainer = styled.nav<{ aniMode: boolean }>`
   width: 40%;
   height: 100%;
   background-color: #000f23;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem;
   position: fixed;
   top: 0;
   left: 0;
@@ -60,6 +78,24 @@ const NavContainer = styled.nav<{ aniMode: boolean }>`
       : css`
           ${SlideOut} ease-in-out 0.3s
         `};
+
+  .nav-list-wrapper {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    gap: 2rem;
+  }
+`;
+
+const NavList = styled.div`
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
+  color: #fff;
+  font-size: 2rem;
+  text-transform: uppercase;
+  cursor: pointer;
 `;
 
 export default NavMenu;

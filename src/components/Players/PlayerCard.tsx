d5ac73a2ec2fbce83a7454cf683c6630e0f90f6c
Rose Jang <jangmi749@gmail.com>
2023-04-02 13:25:17 +0900

    :lipstick: design : 카드 디자인 구현

--- src/components/Players/PlayerCard.tsx
+++ src/components/Players/PlayerCard.tsx
@@ -26,17 +26,25 @@ const PlayerCard = ({
           <h1 className="player-number">{player.number}</h1>
         </CardFront>
         <CardBack>
-          <h1 className="player-name">{player.name}</h1>
-          <img src={player.profileImg} alt={player.name} />
-          <img
+          <div className="player-back-title">
+            <h1 className="player-name">{player.name}</h1>
+            {player.onLoan && (
+              <p className="player-on-loan">On loan to {player.onLoan}</p>
+            )}
+          </div>
+          <div
             className="player-country-flag"
-            src={player.country.flag}
-            alt={player.country.name}
+            style={{ backgroundImage: `url(${player.country.flag})` }}
+          ></div>
+
+          <div
+            className="player-profile"
+            style={{ backgroundImage: `url(${player.profileImg})` }}
           />
-          <p className="player-country-name">{player.country.name}</p>
-          {player.onLoan && (
-            <p className="player-on-loan">on Loan to {player.onLoan}</p>
-          )}
+          <div
+            className="player-profile player-profile-shadow"
+            style={{ backgroundImage: `url(${player.profileImg})` }}
+          />
         </CardBack>
       </div>
     </CardWrapper>
@@ -88,6 +96,7 @@ const CardFront = styled.section`
   align-items: center;
   background-color: #3bd6ff;
   padding: 1rem;
+
   .player-number {
     font-weight: 900;
     font-size: 10rem;
@@ -99,6 +108,48 @@ const CardBack = styled(CardFront)`
   background-color: #fff;
   transform: rotateY(180deg);
   color: #333;
+  padding: 0;
+  padding-top: 2rem;
+  justify-content: space-between;
+  overflow: hidden;
+
+  .player-back-title {
+    text-align: center;
+  }
+  .player-name {
+    font-size: 1.8rem;
+    font-weight: 800;
+  }
+  .player-profile {
+    width: 85%;
+    padding-bottom: 85%;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .player-profile-shadow {
+    position: absolute;
+    bottom: 0;
+    filter: blur(1rem);
+    z-index: -1;
+  }
+  .player-country-flag {
+    width: 20%;
+    padding-bottom: 20%;
+    border-radius: 50%;
+    background-size: cover;
+    background-position: center;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.4);
+    position: absolute;
+    bottom: 5%;
+    right: 5%;
+  }
+
+  @media screen and (max-width: 768px) {
+    .player-name {
+      font-size: 1.4rem;
+    }
+  }
 `;
 
 export default PlayerCard;
